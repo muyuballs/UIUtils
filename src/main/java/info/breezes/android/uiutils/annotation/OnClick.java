32650b36a99c9bfb1ea8978e3333e973160944e9
jianxingqiao <jianxingqiao@gmail.com>
2014-08-14 18:59:00 +0800

    init

---
+++ src/main/java/info/breezes/android/uiutils/annotation/OnClick.java
@@ -0,0 +1,16 @@
+package info.breezes.android.uiutils.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by xbreezes on 14-8-14.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OnClick {
+    int value();
+}
